78ee321690f21850109300eb719a3878be2fc950
inprak02 <inprak02@USCHIZWIMO5324>
2023-11-28 17:39:41 +0530

    1.1

---
+++ CSCLIENT/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Org.OpenAPITools.Api;
+using Org.OpenAPITools.Client;
+using Org.OpenAPITools.Model;
+
+namespace Example
+{
+    public class Example
+    {
+        public static void Main()
+        {
+
+            Configuration config = new Configuration();
+            config.BasePath = "https://localhost:7207/";
+            var apiInstance = new ContactApi(config);
+
+            List<Contact> result = apiInstance.ContactGet();
+            for(int i=0; i<result.Count; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+            var contact = new Contact
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Age = 50
+            };
+            var createdpost = apiInstance.ContactPost(contact);
+            result.Add(createdpost);
+            for (int i = 0; i < result.Count; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+
+
+        }
+    }
+}
